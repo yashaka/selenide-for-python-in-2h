dae6544e386c6d96bd41621b2826ae9c39b82aeb
yashaka <yashaka@gmail.com>
2021-10-13 16:06:45 +0300

    added final assertion to the test, with example of using existing expected condition built into WebDriver package

--- test_google.py
+++ test_google.py
@@ -1,6 +1,7 @@
 import selenide as browser
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as condition
 
 browser.driver = webdriver.Chrome(ChromeDriverManager().install())
 browser.timeout = 2
@@ -13,5 +14,6 @@ query.should_be_blank()
 query.set_value('yashaka selene').press_enter()
 
 browser.element('#rso .g').element('a').click()
+browser.wait().until(condition.url_contains('github.com/yashaka/selene'))
 
 browser.driver.quit()
